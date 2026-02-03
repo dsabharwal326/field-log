070fd0ea30b8319d9fb1bf323d72dc5299159d70
Deepak <dsabh326@icloud.com>
2026-02-02 19:08:41 -0500

    Short message describing change

--- App.tsx
+++ App.tsx
@@ -1,29 +1,31 @@
-import { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import LibraryScreen from './src/screens/LibraryScreen';
-import LogScreen from './src/screens/LogScreen';
-import AddScreen from './src/screens/AddScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
+import LibraryScreen from "./src/screens/LibraryScreen";
+import LogScreen from "./src/screens/LogScreen";
+import AddScreen from "./src/screens/AddScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
 
-import { initDatabase } from './src/db/database';
+import { store, persistor } from "./src/store/store";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  useEffect(() => {
-    initDatabase();
-  }, []);
-
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Library" component={LibraryScreen} />
-        <Tab.Screen name="Log" component={LogScreen} />
-        <Tab.Screen name="Add" component={AddScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Library" component={LibraryScreen} />
+            <Tab.Screen name="Log" component={LogScreen} />
+            <Tab.Screen name="Add" component={AddScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
